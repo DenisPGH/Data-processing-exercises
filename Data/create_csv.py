75964a365341050a65552cb4b927092259c692d8
Denislav <deni_9000_1990@abv.bg>
2022-06-05 16:40:01 +0300

    added binary searching algorithm

--- Data/create_csv.py
+++ Data/create_csv.py
@@ -1,13 +1,16 @@
 import csv
 
+range_a=100000
+range_b=100001
+
 with open('file_a.csv', mode='w',newline='') as employee_file:
     employee_writer = csv.writer(employee_file, delimiter=',')
-    for a in range(10):
+    for a in range(range_a):
         employee_writer.writerow([f'John{a}'])
 
 with open('file_b.csv', mode='w',newline='') as employee_file:
     employee_writer = csv.writer(employee_file, delimiter=',')
-    for a in range(11):
+    for a in range(range_b):
         if a==9:
             continue
         employee_writer.writerow([f'John{a}'])
