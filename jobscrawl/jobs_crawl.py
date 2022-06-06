6b656c623d20c8726b7c1e27a21e308157dd695c
Denislav <deni_9000_1990@abv.bg>
2022-06-06 17:23:50 +0300

    added funk to replace not ascii characters

--- jobscrawl/jobs_crawl.py
+++ jobscrawl/jobs_crawl.py
@@ -63,23 +63,41 @@ result=json.loads(response.text)
 #     csv.write("aaa")
 #     for each_job_ind in range(len(result['documents'])):
 #         csv.write(str(each_job_ind))
+
+def prove_for_german_letter(text):
+    deutsche={'ö':"oe",'Ö':'OE','Ü':'UE','ü':'ue','ä':'ae','Ä':'AE'}
+    for each in deutsche:
+        if each in text:
+            text=text.replace(each,deutsche[each])
+    for each_letter in text:
+        if not 0 <= ord(each_letter) <= 127:
+            text=text.replace(each_letter,'-')
+
+    return str(text)
+
+
+
+
 with open('info.csv', mode='w',newline='') as job_file:
-    jobs_writer = csv.writer(job_file, delimiter=',')
+    jobs_writer = csv.writer(job_file, delimiter='|')
     for each_job_ind in range(len(result['documents'])):
-        title = result["documents"][each_job_ind]['title']
-        publication_date = result["documents"][each_job_ind]['preview']
-        place = result["documents"][each_job_ind]['place']
+        title = prove_for_german_letter(result["documents"][each_job_ind]['title'])
+        print(title)
+        publication_date = result["documents"][0]['publication_date'].split('T')[0]
+        #print(publication_date)
+        place = prove_for_german_letter(result["documents"][each_job_ind]['place'])
         is_active = result["documents"][each_job_ind]['is_active']
-        #link_ = result["documents"][each_job_ind]['_links']['detail_de']['href']
-        jobs_writer.writerow([title,publication_date,place,is_active])
+        link_ = result["documents"][each_job_ind]['_links']['detail_de']['href']
+        #jobs_writer.writerow([title.encode("utf-8")])
+        jobs_writer.writerow([title,publication_date,place,is_active,link_])
 
 
 #print(len(result["documents"][0]))
-link=result["documents"][0]['_links']['detail_de']['href']
+link=result["documents"][0]['_links']['detail_de']['href']#.encode("utf-8")
 title=result["documents"][0]['title']
 id=result["documents"][0]['company_id']
-preview=result["documents"][0]['publication_date']
-publication_date=result["documents"][0]['preview']
+publication_date=result["documents"][0]['publication_date']
+preview=result["documents"][0]['preview']
 place=result["documents"][0]['place']
 is_active=result["documents"][0]['is_active']
 #print(result["documents"][0])
